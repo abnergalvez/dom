3460a76ecd1312348c0864e44b4cd40815fa28f3
Abner <agalvez@mma.gob.cl>
2018-12-13 14:27:30 -0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,16 +38,6 @@ body {
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
      <script type="text/javascript">
-        var body = document.documentElement;
-        if (body.requestFullscreen) {
-            body.requestFullscreen();
-        } else if (body.webkitrequestFullscreen) {
-            body.webkitrequestFullscreen();
-        } else if (body.mozrequestFullscreen) {
-            body.mozrequestFullscreen();
-        } else if (body.msrequestFullscreen) {
-            body.msrequestFullscreen();
-        }
         function groundDetails(zone)
         {
             $.ajax({
