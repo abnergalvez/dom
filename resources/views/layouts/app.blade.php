7748f2c6b0f0851482e899bbcd54a4b08ea70329
Abner <agalvez@mma.gob.cl>
2018-12-13 14:26:09 -0300

    mensajes de error alertas2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=devicewidth, minimal-ui">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
@@ -37,6 +38,16 @@ body {
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
      <script type="text/javascript">
+        var body = document.documentElement;
+        if (body.requestFullscreen) {
+            body.requestFullscreen();
+        } else if (body.webkitrequestFullscreen) {
+            body.webkitrequestFullscreen();
+        } else if (body.mozrequestFullscreen) {
+            body.mozrequestFullscreen();
+        } else if (body.msrequestFullscreen) {
+            body.msrequestFullscreen();
+        }
         function groundDetails(zone)
         {
             $.ajax({
