5fc1903238b1c1638a34839015dc4fc90620da03
Abner <agalvez@mma.gob.cl>
2018-12-17 10:57:46 -0300

    fix https2

--- resources/views/general_map.blade.php
+++ resources/views/general_map.blade.php
@@ -116,7 +116,7 @@
 
     function searchPoligon(loc) {
         $.ajax({
-            url: "https://209.97.156.75:3000/polygons/loc/" + loc.lng + "," + loc.lat,
+            url: "http://209.97.156.75:3000/polygons/loc/" + loc.lng + "," + loc.lat,
             method: 'GET',
             headers: {
               "Content-Type": "application/json",
@@ -154,7 +154,7 @@
 
     function seachAddress(address) {
             $.ajax({
-            url: "https://209.97.156.75:3000/addresses/" + address +", Rancagua",
+            url: "http://209.97.156.75:3000/addresses/" + address +", Rancagua",
             method: 'GET',
             headers: {
               "Content-Type": "application/json",
